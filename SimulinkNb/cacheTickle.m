139440b9d909662dc47382a0877f6c0ffaef3db2
Christopher Wipf <christopher.wipf@LIGO.ORG>
2013-11-08 06:37:33 +0000

    limit the cache size

--- SimulinkNb/cacheTickle.m
+++ SimulinkNb/cacheTickle.m
@@ -11,13 +11,12 @@ for n = 1:size(tickleCache, 1)
     cachedVarargout = tickleCache{n, 2};
     if numel(cachedVarargin) ~= nargin || numel(cachedVarargout) ~= nargout
         continue;
-    end
-    if ~compareOpt(cachedVarargin{1}, varargin{1})
+    elseif ~compareOpt(cachedVarargin{1}, varargin{1})
+        continue;
+    elseif ~all(cellfun(@compareVect, cachedVarargin(2:end), varargin(2:end)))
         continue;
     end
-    if ~all(cellfun(@compareVect, cachedVarargin(2:end), varargin(2:end)))
-        continue;
-    end
+    disp('Reusing Optickle results from a previous run (cached in the global variable ''tickleCache'')');
     varargout = cachedVarargout;
     return;
 end
@@ -28,12 +27,13 @@ end
 
 tickleCache{end+1, 1} = varargin;
 tickleCache{end, 2} = varargout;
+tickleCache = tickleCache(max(end-2, 1):end, :);
 
 end
 
 function out = compareOpt(opt1, opt2) %#ok<INUSD>
 
-out = all(evalc('display(opt1)') == evalc('display(opt2)'));
+out = strcmp(evalc('display(opt1)'), evalc('display(opt2)'));
 
 end
 
