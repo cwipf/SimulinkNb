58a4b50bcede3e3f8e46129f2dc913ad2f8b7c97
Christopher Wipf <christopher.wipf@LIGO.ORG>
2013-12-06 19:03:24 +0000

    better equality testing of Optickle models, using isequaln (thanks Nic and Tobin)

--- SimulinkNb/cacheTickle.m
+++ SimulinkNb/cacheTickle.m
@@ -11,9 +11,7 @@ for n = 1:size(tickleCache, 1)
     cachedVarargout = tickleCache{n, 2};
     if numel(cachedVarargin) ~= nargin || numel(cachedVarargout) ~= nargout
         continue;
-    elseif ~compareOpt(cachedVarargin{1}, varargin{1})
-        continue;
-    elseif ~all(cellfun(@compareVect, cachedVarargin(2:end), varargin(2:end)))
+    elseif ~all(cellfun(@isequaln, cachedVarargin, varargin))
         continue;
     end
     disp('Reusing Optickle results from a previous run (cached in the global variable ''tickleCache'')');
@@ -28,21 +26,5 @@ end
 tickleCache{end+1, 1} = varargin;
 tickleCache{end, 2} = varargout;
 tickleCache = tickleCache(max(end-2, 1):end, :);
-
-end
-
-function out = compareOpt(opt1, opt2) %#ok<INUSD>
-
-out = strcmp(evalc('display(opt1)'), evalc('display(opt2)'));
-
-end
-
-function out = compareVect(v1, v2)
-
-if numel(v1) ~= numel(v2)
-    out = false;
-else
-    out = all(v1 == v2);
-end
 
 end
